bc02a1f986d25ac45596e979a20dba1861fb5d72
guillaumeArrigoni <guillaume.arr@gmail.com>
2022-12-08 23:30:43 +0100

    S1 - Create class Board #3

---
+++ src/Takenoko/Board.java
@@ -0,0 +1,25 @@
+package Takenoko;
+
+import Takenoko.HexagoneBox;
+import Takenoko.Interface.Special;
+import Takenoko.Interface.Color;
+
+public class Board {
+
+
+
+    public Board(){
+        HexagoneBox lac = new HexagoneBox(0,0,0, Color.Lac, Special.Classic);
+
+    }
+
+
+
+
+
+
+
+
+
+
+}

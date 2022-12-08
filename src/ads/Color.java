9b35b4a59692d4a511fd29141399ab36d2afb34f
guillaumeArrigoni <guillaume.arr@gmail.com>
2022-12-08 23:21:03 +0100

    S1 - Create Interface Color and Special #3

---
+++ src/ads/Color.java
@@ -0,0 +1,25 @@
+package ads;
+
+public enum Color {
+    Lac(0, "Lac"),
+    Jaune(1, "Jaune"),
+    Vert(2, "Vert"),
+    Rouge(3, "Rouge");
+    private final int indice;
+
+    private final String description;
+
+    Color(int indice, String description) {
+        this.indice = indice;
+        this.description = description;
+    }
+
+    public int getRank() {
+        return indice;
+    }
+
+    @Override
+    public String toString() {
+        return this.description;
+    }
+}
---
+++ src/ads/Special.java
@@ -0,0 +1,22 @@
+package ads;
+
+public enum Special {
+    Classic(0, "classic");
+    private final int indice;
+
+    private final String description;
+
+    Special(int indice, String description) {
+        this.indice = indice;
+        this.description = description;
+    }
+
+    public int getRank() {
+        return indice;
+    }
+
+    @Override
+    public String toString() {
+        return this.description;
+    }
+}

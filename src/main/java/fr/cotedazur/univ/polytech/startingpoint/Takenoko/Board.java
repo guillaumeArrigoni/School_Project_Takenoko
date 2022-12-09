d9b9ddab431b168693da13304a87233c6d637816
Arthur <arthur.rodriguez640@gmail.com>
2022-12-09 15:54:52 +0100

    tkt #14

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Takenoko/Board.java
@@ -0,0 +1,25 @@
+package main.java.fr.cotedazur.univ.polytech.startingpoint.Takenoko;
+
+import fr.cotedazur.univ.polytech.startingpoint.Takenoko.HexagoneBox;
+import fr.cotedazur.univ.polytech.startingpoint.Takenoko.Interface.Special;
+import fr.cotedazur.univ.polytech.startingpoint.Takenoko.Interface.Color;
+
+public class Board {
+
+
+
+    public Board(){
+        HexagoneBox lac = new HexagoneBox(0,0,0, Color.Lac, Special.Classic);
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Takenoko/Interface/Action.java
@@ -0,0 +1,13 @@
+package main.java.fr.cotedazur.univ.polytech.startingpoint.Takenoko.Interface;
+
+import main.java.fr.cotedazur.univ.polytech.startingpoint.Takenoko.Board;
+import fr.cotedazur.univ.polytech.startingpoint.Takenoko.HexagoneBox;
+
+public class Action {
+
+    public void placeTile(Board board, HexagoneBox tile) {
+
+    }
+
+
+}

ddb493fb19af1c947f97660fb9ccb5d6d1ac42fb
loris <loris.drid@etu.univ-cotedazur.fr>
2022-12-09 16:16:23 +0100

    S1 - Create class MeteoDice #1

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/MeteoDice.java
@@ -0,0 +1,32 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+public class MeteoDice {
+    private final int NBSIDE = 1;
+    enum Meteo {
+        VENT(1),
+        PLUIE(2),
+        ORAGE(3),
+        SOLEIL(4),
+        NUAGES(5),
+        HASARD(6);
+        private final int id;
+        Meteo(int id) {
+            this.id = id;
+        }
+    }
+    public MeteoDice(){
+    }
+
+    public Meteo roll() {
+        int i =  (int) (Math.random() * NBSIDE) + 1;
+        return switch (i) {
+            case 1 -> Meteo.VENT;
+            case 2 -> Meteo.PLUIE;
+            case 3 -> Meteo.ORAGE;
+            case 4 -> Meteo.SOLEIL;
+            case 5 -> Meteo.NUAGES;
+            case 6 -> Meteo.HASARD;
+            default -> null;
+        };
+    }
+}

6a34781f79da814cf3809c1b56416581894263c8
guillaumeArrigoni <guillaume.arr@gmail.com>
2023-01-07 23:19:34 +0100

    S2 - Correct and refactor setGardenerCoords #21

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Takenoko/Board.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Takenoko/Board.java
@@ -69,9 +69,10 @@ public class Board {
         if (box.isIrrigate() && !Arrays.equals(box.getCoordinates(), new int[]{0,0,0})) box.growBamboo();
         HashMap<Integer, int[]> adjacentBox = box.getAdjacentBox();
         ArrayList<HexagoneBox> placedBox = this.getPlacedBox();
-        for (HexagoneBox newBox : placedBox) {
-            if (this.containsValue(adjacentBox, box.getCoordinates())) {
-                if (newBox.isIrrigate() && !Arrays.equals(newBox.getCoordinates(), new int[]{0,0,0})) newBox.growBamboo();
+        for (int[] coordinate : adjacentBox.values()){
+            HexagoneBox newBox = this.getBox.get(HexagoneBox.generateID(coordinate));
+            if (placedBox.contains(newBox) && newBox.isIrrigate() && newBox.getColor() == box.getColor()){
+                newBox.growBamboo();
             }
         }
     }

0647074b7f4d4d7bc4b5ef28da3c49b5c5acaaea
Arthur <arthur.rodriguez640@gmail.com>
2023-02-07 14:55:53 +0100

    première implémentation de opencsv #97

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Takenoko/Main.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Takenoko/Main.java
@@ -1,6 +1,14 @@
 package fr.cotedazur.univ.polytech.startingpoint.Takenoko;
 
 import com.beust.jcommander.Parameter;
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+
+import java.nio.file.*;
 
 import fr.cotedazur.univ.polytech.startingpoint.Takenoko.bot.BotMCTS;
 import fr.cotedazur.univ.polytech.startingpoint.Takenoko.gameArchitecture.hexagoneBox.enumBoxProperties.Color;
@@ -17,17 +25,29 @@ import com.beust.jcommander.JCommander;
 //https://www.redblobgames.com/grids/hexagons/#coordinates
 
 public class Main {
+
+
     @Parameter(names={"--2thousands"}, arity=0)
     boolean twoThousands;
     @Parameter(names={"--demo"},arity=0)
     boolean demo;
-    public static void main(String... args) {
+    @Parameter(names={"--csv"}, arity=0)
+    boolean csv;
+
+    public static void main(String... args) throws IOException {
+        FileSystem fs = FileSystems.getDefault();
+        Path path = fs.getPath("./stats/", "stats.csv");
+        System.out.println(path.toString());
+        File file = new File(path.toString());
+        CSVWriter writer = new CSVWriter(new FileWriter(file));
+
         Main main = new Main();
         JCommander.newBuilder()
                 .addObject(main)
                 .build()
                 .parse(args);
-        if (main.twoThousands) {
+
+        if (main.twoThousands || main.csv) {
             Log log = new Log();
             for (int i = 0; i < 10; i++) {
                 RetrieveBoxIdWithParameters retrieving = new RetrieveBoxIdWithParameters();
@@ -63,8 +83,13 @@ public class Main {
             System.out.println(" -Pourcentage de victoire : " + df.format(winPercentageForBot2) + "%");
             System.out.println(" -Score moyen : " + df.format(meanScoreForBot2));
             System.out.println("------------------------------------------------");
+
+            String[] gameStats = {df.format(winPercentageForBot1), df.format(winPercentageForBot2), df.format(meanScoreForBot1), df.format(meanScoreForBot2)};
+
+            writer.writeNext(gameStats);
+
         }
-        else if (main.demo || !main.twoThousands) {
+        else if (main.demo || (!main.twoThousands && !main.csv)) {
             RetrieveBoxIdWithParameters retrieving = new RetrieveBoxIdWithParameters();
             Board board = new Board(retrieving, 1);
             MeteoDice meteoDice = new MeteoDice();
@@ -79,5 +104,8 @@ public class Main {
             System.out.println(bot1.getBoard().getElementOfTheBoard().getStackOfBox());
             game.play(gestionnaire, "demo");
         }
+
+        writer.close();
+
     }
 }

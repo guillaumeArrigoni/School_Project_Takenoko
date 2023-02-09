50aafcface728e71fb008be1169c5a7a4962ec47
loic pantano <loic.pantano@gmail.com>
2023-02-09 21:33:11 +0100

    Renaming of BotMCTS to botDFS #125

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Takenoko/Main.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Takenoko/Main.java
@@ -9,13 +9,10 @@ import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 
-import java.nio.file.*;
-
 import fr.cotedazur.univ.polytech.startingpoint.Takenoko.Logger.LogInfoDemo;
 import fr.cotedazur.univ.polytech.startingpoint.Takenoko.Logger.LogInfoStats;
-import com.opencsv.ICSVWriter;
 import fr.cotedazur.univ.polytech.startingpoint.Takenoko.bot.Bot;
-import fr.cotedazur.univ.polytech.startingpoint.Takenoko.bot.BotMCTS;
+import fr.cotedazur.univ.polytech.startingpoint.Takenoko.bot.BotDFS;
 import fr.cotedazur.univ.polytech.startingpoint.Takenoko.bot.BotRandom;
 //TODO remettre quand botRuleBased marche
 import fr.cotedazur.univ.polytech.startingpoint.Takenoko.bot.BotRuleBased;
@@ -24,13 +21,12 @@ import fr.cotedazur.univ.polytech.startingpoint.Takenoko.gameArchitecture.hexago
 import fr.cotedazur.univ.polytech.startingpoint.Takenoko.objectives.GestionObjectives;
 import fr.cotedazur.univ.polytech.startingpoint.Takenoko.searching.RetrieveBoxIdWithParameters;
 
-import java.text.DecimalFormat;
 import java.util.*;
 //https://www.redblobgames.com/grids/hexagons/#coordinates
 
 public class Main {
     //parameters for JCommander
-    @Parameter(names={"--2thousands"}, arity=1)
+    @Parameter(names={"--2thousands"}, arity=0)
     boolean twoThousands;
     @Parameter(names={"--demo"},arity=0)
     boolean demo;
@@ -60,7 +56,7 @@ public class Main {
                         gestionnaire.ListOfObjectiveJardinierByDefault(),
                         gestionnaire.ListOfObjectivePandaByDefault()
                 );
-                Bot bot1 = new BotMCTS("Bot1",board,gestionnaire, retrieving, new HashMap<Color,Integer>(),logDemo);
+                Bot bot1 = new BotDFS("Bot1",board,gestionnaire, retrieving, new HashMap<Color,Integer>(),logDemo);
                 Bot bot2 = new BotRuleBased("Bot2",board,random,gestionnaire, retrieving, new HashMap<Color,Integer>(),logDemo);
                 Bot bot3 = new BotRandom("Bot3",board,random,gestionnaire, retrieving, new HashMap<Color,Integer>(),logDemo);
                 Bot bot4 = new BotRandom("Bot4",board,random,gestionnaire, retrieving, new HashMap<Color,Integer>(),logDemo);
@@ -129,7 +125,7 @@ public class Main {
             Board board = new Board(retrieving, 1,2);
             Random random = new Random();
             GestionObjectives gestionnaire = new GestionObjectives(board, retrieving);
-            Bot bot1 = new BotMCTS("Bot1",board,gestionnaire, retrieving, new HashMap<Color,Integer>(),logDemo);
+            Bot bot1 = new BotDFS("Bot1",board,gestionnaire, retrieving, new HashMap<Color,Integer>(),logDemo);
             Bot bot2 = new BotRuleBased("Bot2",board,random,gestionnaire, retrieving, new HashMap<Color,Integer>(),logDemo);
             Bot bot3 = new BotRandom("Bot3",board,random,gestionnaire, retrieving, new HashMap<Color,Integer>(),logDemo);
             Bot bot4 = new BotRandom("Bot4",board,random,gestionnaire, retrieving, new HashMap<Color,Integer>(),logDemo);

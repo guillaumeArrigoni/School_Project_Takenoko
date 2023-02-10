cb854b787fd684fb0ddc0e288ba058e368109c91
guillaumeArrigoni <guillaume.arr@gmail.com>
2023-02-10 03:03:04 +0100

    S9 - Fix bug in FormatterForLog #98

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Takenoko/Logger/FormatterForLog.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Takenoko/Logger/FormatterForLog.java
@@ -8,7 +8,9 @@ public class FormatterForLog extends Formatter {
     private final String ANSI_WHITE_Brighter = "\033[97m";
 
     public static final String ANSI_RED = "\033[1;31m";
+    public static final String ANSI_RED2 = "\u001B[31m";
     public static final String ANSI_ORANGE = "\033[38;5;208m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
 
     private Level level;
 
@@ -21,8 +23,8 @@ public class FormatterForLog extends Formatter {
         StringBuilder builder = new StringBuilder();
         switch (this.level.getName()){
             case "INFO" -> builder.append(ANSI_WHITE_Brighter);
-            case "SEVERE" -> builder.append(ANSI_RED);
-            case "WARNING" -> builder.append(ANSI_RED);
+            case "SEVERE" -> builder.append(ANSI_YELLOW);
+            case "WARNING" -> builder.append(ANSI_RED2);
         }
         builder.append(ANSI_WHITE_Brighter);
         builder.append(record.getMessage());
